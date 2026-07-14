0f0eaa6ae48d6ea84bb5e8edb94d662fb21d2038
TaylorAmarelTech <tamarel.tech@gmail.com>
2026-07-14 10:19:25 -0800

    chore: pin A-00 source commit

--- kaggle/A-00-omni-experiment-workbench/kernel.py
+++ kaggle/A-00-omni-experiment-workbench/kernel.py
@@ -72,7 +72,7 @@ DUECARE_VERSION = os.environ.get("DUECARE_VERSION", "0.17.0")
 DUECARE_REPO = os.environ.get("DUECARE_REPO", "TaylorAmarelTech/gemma4_comp")
 DUECARE_COMMIT_SHA = os.environ.get(
     "DUECARE_COMMIT_SHA",
-    "6d084f77524909ee19c3f7ab915cc9373baca3a7",
+    "b56804906ad45f6def39860b9568b0892f4edbae",
 )
 A00_SMALL_MODEL_REF = os.environ.get("DUECARE_A00_SMALL_MODEL_REF", "google/gemma-4-E2B-it")
 A00_DEFAULT_MODEL_REF = os.environ.get("DUECARE_A00_DEFAULT_MODEL_REF", A00_SMALL_MODEL_REF)
